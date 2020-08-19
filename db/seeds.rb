0034598ff9e636a0471c0ac3559bcde36d357a02
keeks5456 <Akikogreen08@gmail.com>
2020-08-19 12:00:19 -0700

    db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,7 @@
   require 'httparty'
   require 'rest_client'
 
-
-    # class GetAnimeImage
-    #   def self.get_image
-    #     image_url = "https://kitsu.io/api/edge/trending/anime"
-    #     response = HTTParty.get(image_url)
-    #     parsed = JSON.parse(response.body)
-    #   # byebug
-    #   all_Img = parsed['data'].map{|animeImg| animeImg['attributes']['posterImage']['original']}
-    #   #  byebug
-    #   map_Img = all_Img.map {|image| image.to_s }
-    #   #  this returns a string of strings 
-    #   #here we can grab the images by index with map_Img[0] or [2]
-    #   # but will this be possible for our fetch data to do?
-    #   end
-    # end
-    # GetAnimeImage.get_image
-
-
-
-
     class GetAnimeDescription 
-      # def self.get_description
-      #   description = "https://kitsu.io/api/edge/trending/anime"
-      #   response = HTTParty.get(description)
-      #   parsed = JSON.parse(response.body)
-      #   byebug
-      #   all_Text = parsed['data'].map{ |animeTxt| animeTxt['attributes']['synopsis']}
-      #   # map_Text = all_Text.map {|image|  image}
-
-      #   # this gives back an array of strings of the descriptions
-      #   # can be accessed through its index all_text[0] 
-      # # byebug
-      # end
-
       def self.get_description_image
         description = "https://kitsu.io/api/edge/trending/anime"
         response = HTTParty.get(description)
@@ -48,8 +15,6 @@
         all_Text = parsed['data'].map{ |anime| {description: anime['attributes']['synopsis'], image: anime['attributes']['posterImage']['original']}}
       end
     end
-
-
 
     10.times do
       User.create({

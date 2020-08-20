9e7b992cd4efc8ffc74c4b080de7dcd8772a1262
Akiko Green <59545349+keeks5456@users.noreply.github.com>
2020-08-19 23:54:04 -0700

    Add files via upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,29 @@
 class PostsController < ApplicationController
- 
-end
+  
+  def index 
+    @posts = Post.all 
+    options = {
+      include: [:user, :comment]
+    }
+    render json: PostSerializer.new(@posts, options)
+  end
+
+  def show 
+    @post = Post.find_by(params[:id])
+    options = {
+      include: [:user, :comment]
+    }
+    render json: PostSerializer.new(@post, options)
+  end
+
+  def update 
+    @post = Post.find_by(params[:id])
+    @post.update(params.require(:post).permit(:likes))
+    @post.save
+   
+    render json: @post
+  end
+
+
+  # end of class Post
+end 
